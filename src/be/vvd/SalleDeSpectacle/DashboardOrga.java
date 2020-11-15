f08de70bce81038379f5ff17a8996290d5a32938
Dylan Van Vlaender <dylan.vanvlaender@hotmail.com>
2020-11-15 15:24:57 +0100

    Ajout de fonctionnalité qui trie les jours disponibles dans le calendrier

--- src/be/vvd/SalleDeSpectacle/DashboardOrga.java
+++ src/be/vvd/SalleDeSpectacle/DashboardOrga.java
@@ -7,6 +7,7 @@ import java.sql.Date;
 import java.awt.EventQueue;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Point;
 import java.awt.Toolkit;
 
 import javax.swing.JFrame;
@@ -14,8 +15,13 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.awt.event.ActionEvent;
 import com.toedter.calendar.JDayChooser;
 import com.toedter.calendar.JCalendar;
@@ -72,6 +78,66 @@ public class DashboardOrga extends JFrame {
 		contentPane.add(btnDeconnexion);
 		
 		JCalendar calendar = new JCalendar();
+		calendar.addPropertyChangeListener(new PropertyChangeListener() {
+	        @Override
+	        public void propertyChange(PropertyChangeEvent e) {
+	        	Component comp[] = calendar.getDayChooser().getDayPanel().getComponents();
+	        	
+	        	int day = calendar.getDayChooser().getDay();
+	    		int month = calendar.getMonthChooser().getMonth();		
+	    		int year = calendar.getYearChooser().getYear();
+	    		
+	    		
+	    		
+	    		String str = "30-11-2020";
+	    		
+	    		String daydb = str.substring(0,2);
+	    		String monthdb = str.substring(3,5);
+	    		String yeardb = str.substring(6,10);
+	    		if(daydb.lastIndexOf('0')==0) {
+	    			daydb = daydb.substring(1,2);
+	    		}
+	    		
+	    		System.out.println(daydb);
+	    		System.out.println(monthdb);
+	    		System.out.println(yeardb);
+	    		System.out.println(month);
+	    		System.out.println(year);
+	    		
+	    		 Calendar cal = Calendar.getInstance();
+	    	     cal.set(Calendar.MONTH, month);
+	    	     cal.set(Calendar.YEAR, year);
+	    	     cal.set(Calendar.DAY_OF_MONTH, 1);
+	    	     java.util.Date firstDayOfMonth = cal.getTime();  
+	    	
+	    	     DateFormat sdf = new SimpleDateFormat("EEEEEEEE");   
+	    	     String strfirstDayOMonth = sdf.format(firstDayOfMonth);
+	    	     
+	    	     System.out.println(strfirstDayOMonth);
+	    	     
+	    	     int j=0;
+	    	     
+	    	     switch(strfirstDayOMonth) {
+	    	     	case "Sunday" -> j=0;
+	    	     	case "Monday" -> j=1;
+	    	     	case "Tuesday" -> j=2;
+	    	     	case "Wednesday" -> j=3;
+	    	     	case "Thursday" -> j=4;
+	    	     	case "Friday" -> j=5;
+	    	     	case "Saturday" -> j=6;
+	    	     }
+	    		
+	    		for(int i = 7+j ; i<comp.length;i++) {
+	    			if(Integer.parseInt(yeardb)==year && Integer.parseInt(monthdb)==month+1) {
+	    				if(i-((7+j)+1)==Integer.parseInt(daydb)) {
+	    					comp[i-2].setBackground(new Color(255,0,0));
+	    					comp[i-2].setEnabled(false);
+	    				}
+	    			}
+	    		}
+	    		
+	        }
+	    });
 		calendar.setBounds(10, 41, 206, 152);
 		
 		contentPane.add(calendar);
@@ -89,12 +155,8 @@ public class DashboardOrga extends JFrame {
 				}
 				java.sql.Date date = new java.sql.Date(parsed.getTime());
 				Representation rep = new Representation(date);
-				Component comp[] = calendar.getDayChooser().getDayPanel().getComponents();
-				for(int i = 7 ; i< comp.length; i++) {
-					comp[i].setBackground(new Color(255,0,0));
-					comp[i].setEnabled(false);
-				}
-//				rep.ajouterRepresentation();
+				
+				rep.ajouterRepresentation();
 			}
 		});
 		btnNewButton.setBounds(72, 203, 85, 21);
